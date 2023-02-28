f42e4a287eac7a58809f08b926edbe30dda5a299
Boof <97455552+hexahigh@users.noreply.github.com>
2023-02-28 12:29:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
-sudo apt update
-sudo apt install imagemagick
-sudo apt install libzbar-dev
-sudo apt install qrencode
+echo What package manager do you use?
+echo APT
+echo PACMAN
+read packageman
+if [packageman = APT ] then
+  sudo apt update
+  sudo apt install imagemagick
+  sudo apt install libzbar-dev
+  sudo apt install qrencode
+fi
+
+if [packageman = PACMAN ] then
+  sudo pacman -S imagemagick libzbar-dev qrencode
+fi
