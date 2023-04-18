e574e49b3867743ff0f41dd317650117989df73b
Boof <97455552+hexahigh@users.noreply.github.com>
2023-04-18 06:45:37 +0200

    Added setup.sh (#26)
    * Create setup.sh
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Update setup.sh
    
    * add yum
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Make package manager detection automatic
    
    ---------
    
    Co-authored-by: Alisina Bahadori <alisina.bm@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+detect_os() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "linux"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "macos"
+  else
+    echo "unknown"
+  fi
+}
+
+detect_package_manager() {
+  local os=$1
+  if [ "$os" = "linux" ]; then
+    if command -v apt &> /dev/null; then
+      echo "apt"
+    elif command -v pacman &> /dev/null; then
+      echo "pacman"
+    elif command -v yum &> /dev/null; then
+      echo "yum"
+    else
+      echo "unknown"
+    fi
+  elif [ "$os" = "macos" ]; then
+    if command -v brew &> /dev/null; then
+      echo "brew"
+    else
+      echo "unknown"
+    fi
+  else
+    echo "unknown"
+  fi
+}
+
+run_sudo_command() {
+  if command -v sudo &> /dev/null; then
+    sudo "$@"
+  else
+    "$@"
+  fi
+}
+
+os=$(detect_os)
+packageman=$(detect_package_manager "$os")
+
+echo "OS: $os Package Manager: $packageman"
+
+if [ "$packageman" = "apt" ]; then
+  run_sudo_command apt update
+  run_sudo_command apt install -y imagemagick libzbar-dev qrencode
+elif [ "$packageman" = "pacman" ]; then
+  run_sudo_command pacman -Syu imagemagick zbar qrencode
+elif [ "$packageman" = "yum" ]; then
+  run_sudo_command yum install -y imagemagick zbar-devel qrencode
+elif [ "$packageman" = "brew" ]; then
+  brew install imagemagick zbar qrencode
+else
+  echo "Unable to detect a supported package manager."
+fi
